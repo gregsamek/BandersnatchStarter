d7a1dd2fae5355c5c6c5ffc308b4160b35b07112
Greg Samek <gregsamek@users.noreply.github.com>
2023-11-21 21:22:30 -0500

    Database Interface Implementation
    Develop a database interface class
    Create random monster data with the MonsterLab library and populate the database

--- app/data.py
+++ app/data.py
@@ -6,20 +6,32 @@ from MonsterLab import Monster
 from pandas import DataFrame
 from pymongo import MongoClient
 
-
 class Database:
 
-    def seed(self, amount):
-        pass
+    def __init__(self):
+        load_dotenv()
+        self.database = MongoClient(getenv("DB_URL"), tlsCAFile=where())["Bandersnatch"]
+        self.collection = self.database.get_collection("Monsters")
 
+    def seed(self, amount):
+        """insert the specified number of documents into the collection"""
+        self.collection.insert_many([Monster().to_dict() for _ in range(amount)])
+    
     def reset(self):
-        pass
+        """deletes all documents from the collection"""
+        self.collection.delete_many({})
 
     def count(self) -> int:
-        pass
+        """returns the number of documents in the collection"""
+        return self.collection.count_documents({})
 
     def dataframe(self) -> DataFrame:
-        pass
+        """returns a DataFrame containing all documents in the collection"""
+        return DataFrame(self.collection.find())
 
     def html_table(self) -> str:
-        pass
+        """returns an HTML table representation of the DataFrame (or None)"""
+        return self.dataframe().to_html() if self.count() else None
+
+if __name__ == '__main__':
+    db = Database()
