40ccfcadf984324c1c2b51328464a2c1045291ff
Greg Samek <gregsamek@users.noreply.github.com>
2024-01-01 21:42:26 -0500

    Sprint 3: Machine Class

--- app/machine.py
+++ app/machine.py
@@ -1,17 +1,30 @@
+from pandas import DataFrame
+from sklearn.linear_model import LogisticRegression
+import joblib
+from datetime import datetime
+
 class Machine:
 
-    def __init__(self, df):
-        pass
+    def __init__(self, df: DataFrame):
+        self.name = "Logistic Regression Classifier"
+        self.timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S %p")
+        
+        self.model = LogisticRegression()
+        X = df[['Level', 'Health', 'Energy', 'Sanity']]
+        y = df['Rarity']
+        self.model.fit(X, y)
 
-    def __call__(self, feature_basis):
-        pass
+    def __call__(self, pred_basis: DataFrame):
+        prediction, *_ = self.model.predict(pred_basis)
+        confidence = self.model.predict_proba(pred_basis).max()
+        return prediction, confidence
 
     def save(self, filepath):
-        pass
+        joblib.dump(self, filepath)
 
     @staticmethod
     def open(filepath):
-        pass
+        return joblib.load(filepath)
 
     def info(self):
-        pass
+        return f"Base Model: {self.name} <br/> Timestamp: {self.timestamp}"
